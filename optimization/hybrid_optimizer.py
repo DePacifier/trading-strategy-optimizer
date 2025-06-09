7a176d7ed1280c937cd600eab4868448b1d95c0b
Milky Bekele <37738957+DePacifier@users.noreply.github.com>
2025-06-09 19:04:43 +0300

    Improve ParallelHybridOptimizer

--- optimization/hybrid_optimizer.py
+++ optimization/hybrid_optimizer.py
@@ -8,66 +8,37 @@ class ParallelHybridOptimizer(Optimizer):
         self.pso_optimizer = pso_optimizer
         self.bayesian_optimizer = bayesian_optimizer
         self.de_optimizer = differential_optimizer
-        self.n_processes = n_processes or 10
+        self.n_processes = n_processes or mp.cpu_count()
+
+    def _best(self, results, objective_function):
+        """Return the best parameter set based on the objective function."""
+        scored = [(objective_function(r), r) for r in results]
+        scored.sort(key=lambda x: x[0], reverse=True)
+        return scored[0][1]
 
     def optimize(self, objective_function, param_ranges, n_iterations):
         with mp.Pool(processes=self.n_processes) as pool:
-            # Parallel GA optimization
-            print("Started GA Optimization")
-            ga_results = pool.starmap(self.ga_optimizer.optimize, 
-                                      [(objective_function, param_ranges, n_iterations // 3)] * self.n_processes)
-            
-            # Handle multi-objective results
-            if isinstance(ga_results[0][0], (list, np.ndarray)):
-                ga_results.sort(key=lambda x: np.sum(x), reverse=True)
-                ga_best = ga_results[:5]
-            else:
-                ga_results.sort(key=objective_function, reverse=True)
-                ga_best = ga_results[:5]
-                
-                
-            print("Finished GA Optimization\nResult:\n", ga_best)
+            phase_iters = max(1, n_iterations // 4)
 
-            # print("Finished GA Optimization\nResult:\n", ga_best)
-            # # Convert the best GA result to an initial guess for PSO
-            # initial_guess = [ga_best]
-            
-            # # Parallel PSO optimization
-            # print("Started PSO Optimization")
-            # pso_results = pool.starmap(self.pso_optimizer.optimize, 
-            #                            [(objective_function, param_ranges, n_iterations // 3), initial_guess] * self.n_processes)
-            # pso_best = max(pso_results, key=objective_function)
-            # print("Finished PSO Optimization\nResult:\n", pso_best)
-            
-            # # Parallel Bayesian optimization
-            # print("Started Bayesian Optimization")
-            # bayesian_results = pool.starmap(self.bayesian_optimizer.optimize, 
-            #                                 [(objective_function, param_ranges, n_iterations // 3, pso_best)] * self.n_processes)
-            # final_best = max(bayesian_results, key=objective_function)
-            # print("Finished Bayesian Optimization\nResult:\n", final_best)
-            
-            # Parallel DE optimization
-            print("Started Differential Optimization")
-            de_results = pool.starmap(self.de_optimizer.optimize, 
-                                       [(objective_function, param_ranges, n_iterations // 3, ga_best)] * self.n_processes)
-            # Handle multi-objective results
-            if isinstance(de_results[0][0], (list, np.ndarray)):
-                de_best = min(de_results, key=lambda x: np.sum(x))
-            else:
-                de_best = max(de_results, key=objective_function)
-            print("Finished Differential Optimization\nResult:\n", de_best)
+            # ---- Genetic Algorithm ----
+            ga_args = (objective_function, param_ranges, phase_iters)
+            ga_results = pool.starmap(self.ga_optimizer.optimize, [ga_args] * self.n_processes)
+            ga_best = self._best(ga_results, objective_function)
 
-            # Parallel Bayesian optimization
-            print("Started Bayesian Optimization")
-            bayesian_results = pool.starmap(self.bayesian_optimizer.optimize, 
-                                            [(objective_function, param_ranges, n_iterations // 3, list(de_best))] * self.n_processes)
-            
-            # Handle multi-objective results
-            if isinstance(bayesian_results[0][0], (list, np.ndarray)):
-                final_best = min(bayesian_results, key=lambda x: np.sum(x))
-            else:
-                final_best = max(bayesian_results, key=objective_function)
-            
-            print("Finished Bayesian Optimization\nResult:\n", final_best)
+            # ---- Particle Swarm ----
+            pso_args = (objective_function, param_ranges, phase_iters, ga_best)
+            pso_results = pool.starmap(self.pso_optimizer.optimize, [pso_args] * self.n_processes)
+            pso_best = self._best(pso_results, objective_function)
+
+            # ---- Differential Evolution ----
+            de_args = (objective_function, param_ranges, phase_iters)
+            de_results = pool.starmap(self.de_optimizer.optimize, [de_args] * self.n_processes)
+            de_best = self._best(de_results, objective_function)
+
+            # ---- Bayesian Optimization ----
+            bayes_iters = max(6, phase_iters)
+            bayes_args = (objective_function, param_ranges, bayes_iters, de_best)
+            bayes_results = pool.starmap(self.bayesian_optimizer.optimize, [bayes_args] * self.n_processes)
+            final_best = self._best(bayes_results, objective_function)
 
         return final_best
